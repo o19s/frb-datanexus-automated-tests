c1adde775f275de1901aba1a5c0c4b442ea5d3a4
Bertrand Rigaldies <brigaldies@gmail.com>
2018-05-08 16:05:02 -0400

    Cont'd experimenting with a tester of the number of matches (grouped and ungrouped).

--- src/tester_match_counts.js
+++ src/tester_match_counts.js
@@ -1,6 +1,8 @@
-pm.test("Number of matches", function () {
-    console.log('matches = ' + pm.response.json().grouped.$grouping_value.matches);
-    console.log('ngroups = ' + pm.response.json().grouped.$grouping_value.ngroups);
-    pm.expect(pm.response.json().grouped.$grouping_value.matches).to.equal(expected_matches);
-    pm.expect(pm.response.json().grouped.$grouping_value.ngroups).to.equal(expected_matches_grouped);
-});
+function tester_match_counts(expected_matches, expected_matches_grouped) {
+    pm.test("Number of matches", function () {
+        console.log('matches = ' + pm.response.json().grouped.$grouping_value.matches);
+        console.log('ngroups = ' + pm.response.json().grouped.$grouping_value.ngroups);
+        pm.expect(pm.response.json().grouped.$grouping_value.matches).to.equal(expected_matches);
+        pm.expect(pm.response.json().grouped.$grouping_value.ngroups).to.equal(expected_matches_grouped);
+    });
+}
